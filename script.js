67f44da65e909dc6a36790bbd42de2f2816a715a
Dennis Riemer <dennis.riemer@tradebyte.com>
2019-09-30 16:07:26 +0200

    changed script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function popup() {
-    alert("Hello");
+var txt;
+  var person = prompt("Please enter your name:", "Harry Potter");
+  if (person == null || person == "") {
+    txt = "User cancelled the prompt.";
+  } else {
+    txt = "Hello " + person + "! How are you today?";
+  }
+  alert(txt);
 }
