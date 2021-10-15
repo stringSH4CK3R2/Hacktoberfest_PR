6dabf25086d15706349dbceb3cf0928346368b19
Dennis Riemer <dennisriemer-12@web.de>
2021-10-15 10:41:43 +0200

    fix js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-var msg = document.getElementById("msg");
+var caption = document.getElementById("caption");
 
 function popup() {
 var txt;
-  var person = prompt("Please enter your name:", "Harry Potter");
-  if (person == null || person == "") {
-    msg.innerHTML = "SOMETHING WENT WRONG"
-  } else {
-    txt = "Hello " + person + "! How are you today?";
-    msg.innerHTML = "Hi, "+ person;
+  var name = prompt("Please enter your name:", "Harry Potter");
+  if (name != null && name != "") {
+    caption.innerHTML = "Hi, "+ name;
   }
 }
